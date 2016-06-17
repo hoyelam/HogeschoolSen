bd540955b111755937b29da69a2fdee5d19b2186
Hoye Lam <hoyelam96@gmail.com>
2016-06-17 13:50:56 +0200

    Decorator for button

---
+++ HogeschoolSen/Buttons/ButtonDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogeschoolSen.Buttons
+{
+    public abstract class ButtonDecorator : AbstractButton
+    {
+        protected AbstractButton _button;
+
+        public ButtonDecorator(AbstractButton button)
+        {
+            _button = button;
+        }
+
+
+    }
+}
---
+++ HogeschoolSen/Buttons/SizeAdjustableDecorator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HogeschoolSen.Buttons
+{
+    public class SizeAdjustableDecorator : ButtonDecorator
+    {
+        private int _height = 40;
+        private int _width = 40;
+
+        public SizeAdjustableDecorator(AbstractButton button) : base(button)
+        {
+        }
+
+        public void AdjustHeight(int heightIncreaseBy)
+        {
+            _height += heightIncreaseBy;
+        }
+
+        public void AdjustWidth(int widthIncreaseBy)
+        {
+            _width += widthIncreaseBy;
+        }
+
+        public string GetHeight()
+        {
+            return _height.ToString();
+        }
+
+        public string GetWidth()
+        {
+            return _width.ToString();
+        }
+    }
+}
